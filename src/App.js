57bab4a5b1340a639e936f21d2f85f4a11172a48
Kelly <devkells00@gmail.com>
2018-10-28 15:54:06 -0500

    Done

--- src/App.js
+++ src/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import React, { Component } from 'react';
+import Button from './components/Button'
+import ExampleComponent from './components/ExampleComponent'
+import Greeting from './components/Greeting'
+ class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      peopleInSpace: []
+    }
+  }
+   render () {
+    return (
+      <div>
+        <Greeting />
+        <ExampleComponent info={this.state.peopleInSpace}/><br></br>
+        <Button />
+      </div>
+    )
+  }
+   componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+    .then(resp => resp.json())
+    .then(data => {
+      this.setState({ peopleInSpace: data.people } )
+    })
+  }
+}
+ export default App
--- src/components/ExampleComponent.js
+++ src/components/ExampleComponent.js
@@ -2,14 +2,23 @@ import React, { Component } from 'react';
 
 class ExampleComponent extends Component {
 
-  render() {
-    return (
-      <strong>
-        Example!
-      </strong>
-    )
+ generatePeopleDivs = () => {
+    return this.props.info.map(person => {
+      return (
+        <div>
+          <h2>Name: {person.name}</h2>
+          <h3>Craft: {person.craft}</h3>
+        </div>
+      )
+    })
   }
-
+render() {
+  return (
+    <strong>
+      {this.generatePeopleDivs()}
+    </strong>
+   )
+ }
+ 
 }
-
 export default ExampleComponent;
